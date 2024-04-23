552671b2735efa254216d6e32eace662ff793a13
ddmtchr <rmr04489@gmail.com>
2024-04-23 16:17:35 +0300

    fixes lab4

--- scripts/script4.js
+++ scripts/script4.js
@@ -4,8 +4,9 @@ const matrixInput = document.querySelector('#matrix-input')
 const fileInput = document.querySelector('#file-input')
 const solution = document.querySelector('#out-solution')
 const downloadButton = document.querySelector('#download-button')
-const approximationNames = ['линейная', 'квадратичная', 'кубическая', 'экспоненциальная', 'логарифмическая', 'степенная']
-const approximationColors = ['красный', 'синий', 'зеленый', 'фиолетовый', 'оранжевый', 'черный']
+const approximationNames = ['линейная', 'квадратичная', 'кубическая', null, null, null]
+const approximationColors = ['красный', 'синий', 'зеленый', null, null, null]
+const latexStrings = Array(6).fill(null)
 let fileText = ''
 
 pointsNumberInput.addEventListener('input', toggleMatrix)
@@ -14,11 +15,12 @@ const calculatorDiv = document.getElementById('calculator');
 const calculator = Desmos.GraphingCalculator(calculatorDiv, {
     // expressions: true,
     // settingsMenu: true,
-    // zoomButtons: true,
+    zoomButtons: true,
+    lockViewport: false,
     expressions: false,
-    settingsMenu: false,
-    zoomButtons: false,
-    lockViewport: true
+    settingsMenu: false
+    // zoomButtons: false,
+    // lockViewport: true
 })
 
 fileInput.addEventListener('change', function () {
@@ -74,14 +76,16 @@ function toggleMatrix() {
 }
 
 function clearInput() {
-    let inputs = document.getElementsByTagName('input')
-    for (let i = 0; i < inputs.length; i++) {
-        inputs[i].value = ''
+    let matrixInputs = document.querySelectorAll('#matrix-input input')
+    let matrixInputsList = [...matrixInputs]
+    for (let i = 0; i < matrixInputsList.length; i++) {
+        matrixInputsList[i].value = ''
     }
 }
 
 function clearOutput() {
     solution.innerHTML = ''
+    calculator.setBlank()
 }
 
 function calculate() {
@@ -93,6 +97,7 @@ function calculate() {
         return
     }
     let [pointsX, pointsY] = parseMatrix();
+    console.log(pointsX, pointsY)
     if (!validateFullMatrix(pointsX) || !validateFullMatrix(pointsY)) {
         setOriginal('Точки заполнены не полностью')
         return
@@ -100,22 +105,16 @@ function calculate() {
 
     const [xmin, xmax] = [Math.min(...pointsX), Math.max(...pointsX)]
     const ymin = Math.min(...pointsY)
-    if (xmin <= 0 || ymin <= 0) {
-        setOriginal('Все координаты должны быть положительными')
-        return
-    }
 
     const linearCoefs = linearApproximation(pointsX, pointsY)
     const quadraticCoefs = quadraticApproximation(pointsX, pointsY)
     const cubicCoefs = cubicApproximation(pointsX, pointsY)
-    const expCoefs = exponentialApproximation(pointsX, pointsY)
-    const logCoefs = logarithmicApproximation(pointsX, pointsY)
-    const powerCoefs = powerApproximation(pointsX, pointsY)
+    let expCoefs = null, logCoefs = null, powerCoefs = null
 
-    const latexStrings = [`${linearCoefs[0]}x+(${linearCoefs[1]})`,
-        `${quadraticCoefs[0]}x^2+(${quadraticCoefs[1]})x+(${quadraticCoefs[2]})`,
-        `${cubicCoefs[0]}x^3+(${cubicCoefs[1]})x^2+(${cubicCoefs[2]})x+(${cubicCoefs[3]})`,
-        `${expCoefs[0]}*e^{${expCoefs[1]}x}`, `${logCoefs[0]}\\ln(x)+${logCoefs[1]}`, `${powerCoefs[0]}x^{${powerCoefs[1]}}`]
+    latexStrings[0] = `${linearCoefs[0]}x+(${linearCoefs[1]})`
+    latexStrings[1] = `${quadraticCoefs[0]}x^2+(${quadraticCoefs[1]})x+(${quadraticCoefs[2]})`
+    latexStrings[2] = `${cubicCoefs[0]}x^3+(${cubicCoefs[1]})x^2+(${cubicCoefs[2]})x+(${cubicCoefs[3]})`
+    // ,{id: '7', latex: '\\ln(x)+1', color: '#ffff00'}
 
     calculator.setMathBounds({
         left: xmin - 1,
@@ -133,64 +132,67 @@ function calculate() {
             id: '2',
             latex: latexStrings[2],
             color: Desmos.Colors.GREEN
-        },
-        {id: '3', latex: latexStrings[3], color: Desmos.Colors.PURPLE},
-        {id: '4', latex: latexStrings[4], color: Desmos.Colors.ORANGE},
-        {id: '5', latex: latexStrings[5], color: Desmos.Colors.BLACK}
-        // ,{id: '7', latex: '\\ln(x)+1', color: '#ffff00'}
-    ])
+        }])
+
+    if (xmin > 0 && ymin > 0) powerCoefs = addApproximation('power', pointsX, pointsY)
+    if (ymin > 0) expCoefs = addApproximation('exponential', pointsX, pointsY)
+    if (xmin > 0) logCoefs = addApproximation('logarithmic', pointsX, pointsY)
+
     calculatorDiv.hidden = false;
 
     let approximatedY = [[], [], [], [], [], []]
-    let averageYApproximated = []
+
+    let averageYApproximated = Array(6).fill(null)
     for (let i = 0; i < pointsNumberValue; i++) {
         approximatedY[0].push(linear(linearCoefs[0], linearCoefs[1], pointsX[i]))
         approximatedY[1].push(quadratic(quadraticCoefs[0], quadraticCoefs[1], quadraticCoefs[2], pointsX[i]))
         approximatedY[2].push(cubic(cubicCoefs[0], cubicCoefs[1], cubicCoefs[2], cubicCoefs[3], pointsX[i]))
-        approximatedY[3].push(exponential(expCoefs[0], expCoefs[1], pointsX[i]))
-        approximatedY[4].push(logarithmic(logCoefs[0], logCoefs[1], pointsX[i]))
-        approximatedY[5].push(power(powerCoefs[0], powerCoefs[1], pointsX[i]))
+        if (expCoefs !== null) approximatedY[3].push(exponential(expCoefs[0], expCoefs[1], pointsX[i]))
+        if (logCoefs !== null) approximatedY[4].push(logarithmic(logCoefs[0], logCoefs[1], pointsX[i]))
+        if (powerCoefs !== null) approximatedY[5].push(power(powerCoefs[0], powerCoefs[1], pointsX[i]))
     }
     for (let i = 0; i < 6; i++) {
-        averageYApproximated.push([...approximatedY[i]].reduce((a, b) => a + b, 0) / pointsNumberValue)
+        if (approximatedY[i].length !== 0) {
+            averageYApproximated[i] = [...approximatedY[i]].reduce((a, b) => a + b, 0) / pointsNumberValue
+        }
     }
 
-    const functionsLabel = document.createElement('p')
-    functionsLabel.innerText += `Аппроксимирующие функции:\n 
-    \\(${linearCoefs[0]}x+(${linearCoefs[1]})\\)\n
-    \\(${quadraticCoefs[0]}x^2+(${quadraticCoefs[1]})x+(${quadraticCoefs[2]})\\)\n
-    \\(${cubicCoefs[0]}x^3+(${cubicCoefs[1]})x^2+(${cubicCoefs[2]})x+(${cubicCoefs[3]})\\)\n
-    \\(${expCoefs[0]}*e^{${expCoefs[1]}x}\\)\n
-    \\(${logCoefs[0]}\\ln(x)+${logCoefs[1]}\\)\n
-    \\(${powerCoefs[0]}x^{${powerCoefs[1]}}\\)\n
+    const functionsLabel = document.createElement('div')
+    functionsLabel.innerHTML += `<p>Аппроксимирующие функции:</p>
+    <p>\\(${linearCoefs[0]}x+(${linearCoefs[1]})\\)</p>
+    <p>\\(${quadraticCoefs[0]}x^2+(${quadraticCoefs[1]})x+(${quadraticCoefs[2]})\\)</p>
+    <p>\\(${cubicCoefs[0]}x^3+(${cubicCoefs[1]})x^2+(${cubicCoefs[2]})x+(${cubicCoefs[3]})\\)</p>
     `
+    if (expCoefs !== null) functionsLabel.innerHTML += `<p>\\(${expCoefs[0]}*e^{${expCoefs[1]}x}\\)</p>`
+    if (logCoefs !== null) functionsLabel.innerHTML += `<p>\\(${logCoefs[0]}\\ln(x)+(${logCoefs[1]})\\)</p>`
+    if (powerCoefs !== null) functionsLabel.innerHTML += `<p>\\(${powerCoefs[0]}x^{${powerCoefs[1]}}\\)</p>`
     solution.appendChild(functionsLabel)
 
     MathJax.typeset()
 
-    let deviations = [[], [], [], [], [], []], quadDeviations = []
-    let standardDeviations = []
-    let determinations = []
+    let deviations = Array(6).fill([]), quadDeviations = Array(6).fill(null)
+    let standardDeviations = Array(6).fill(null), determinations = Array(6).fill(null)
 
     for (let i = 0; i < 6; i++) {
-        let quadDeviation = 0
-        let deviationFromAverage = 0
-        let determination = 0
-        for (let j = 0; j < pointsNumberValue; j++) {
-            deviations[i].push(approximatedY[i][j] - pointsY[j])
-            quadDeviation += Math.pow(approximatedY[i][j] - pointsY[j], 2)
-            deviationFromAverage += Math.pow(averageYApproximated[i] - pointsY[j], 2)
+        if (averageYApproximated[i] !== null) {
+            let quadDeviation = 0
+            let deviationFromAverage = 0
+            let determination = 0
+            for (let j = 0; j < pointsNumberValue; j++) {
+                deviations[i].push(approximatedY[i][j] - pointsY[j])
+                quadDeviation += Math.pow(approximatedY[i][j] - pointsY[j], 2)
+                deviationFromAverage += Math.pow(averageYApproximated[i] - pointsY[j], 2)
+            }
+            quadDeviations[i] = quadDeviation
+            determination = 1 - quadDeviation / deviationFromAverage
+            determinations[i] = determination
+            standardDeviations[i] = Math.sqrt(quadDeviation / pointsNumberValue)
         }
-        quadDeviations.push(quadDeviation)
-        determination = 1 - quadDeviation / deviationFromAverage
-        determinations.push(determination)
-        let standardDeviation = Math.sqrt(quadDeviation / pointsNumberValue)
-        standardDeviations.push(standardDeviation)
     }
 
     solution.appendChild(createStatTable(quadDeviations, standardDeviations, determinations))
 
-    const minDeviation = Math.min(...standardDeviations)
+    const minDeviation = Math.min(...standardDeviations.filter((x) => x !== null))
     const bestApproximationIndex = standardDeviations.findIndex(x => x === minDeviation)
     const bestApproximationLabel = document.createElement('p')
     bestApproximationLabel.innerText += `Лучшая аппроксимация - ${approximationNames[bestApproximationIndex]} (${approximationColors[bestApproximationIndex]} график)
@@ -346,6 +348,32 @@ function validateFullMatrix(m) {
     return !m.includes(NaN)
 }
 
+function addApproximation(name, pointsX, pointsY) {
+    let coefs = null
+    if (name === 'exponential') {
+        coefs = exponentialApproximation(pointsX, pointsY)
+        latexStrings[3] = `${coefs[0]}*e^{${coefs[1]}x}`
+        calculator.setExpression({id: '3', latex: latexStrings[3], color: Desmos.Colors.PURPLE})
+        approximationNames[3] = 'экспоненциальная'
+        approximationColors[3] = 'фиолетовый'
+    }
+    if (name === 'logarithmic') {
+        coefs = logarithmicApproximation(pointsX, pointsY)
+        latexStrings[4] = `${coefs[0]}\\ln(x)+${coefs[1]}`
+        calculator.setExpression({id: '4', latex: latexStrings[4], color: Desmos.Colors.ORANGE})
+        approximationNames[4] = 'логарифмическая'
+        approximationColors[4] = 'оранжевый'
+    }
+    if (name === "power") {
+        coefs = powerApproximation(pointsX, pointsY)
+        latexStrings[5] = `${coefs[0]}x^{${coefs[1]}}`
+        calculator.setExpression({id: '5', latex: latexStrings[5], color: Desmos.Colors.BLACK})
+        approximationNames[5] = 'степенная'
+        approximationColors[5] = 'черный'
+    }
+    return coefs
+}
+
 function parseFile(text) {
     const parsed = text.trim().split(/\s+/)
     for (let i = 0; i < parsed.length; i++) {
@@ -401,18 +429,24 @@ function createStatTable(S, sko, det) {
     let headerCell1 = headerRow.insertCell();
     let headerCell2 = headerRow.insertCell();
     let headerCell3 = headerRow.insertCell();
-    headerCell1.textContent = 'Мера отклонения';
-    headerCell2.textContent = 'СКО';
-    headerCell3.textContent = 'Достоверность';
+    let headerCell4 = headerRow.insertCell();
+    headerCell1.textContent = 'Функция';
+    headerCell2.textContent = 'Мера отклонения';
+    headerCell3.textContent = 'СКО';
+    headerCell4.textContent = 'Достоверность';
 
     for (let i = 0; i < S.length; i++) {
-        let row = table.insertRow();
-        let cell1 = row.insertCell();
-        let cell2 = row.insertCell();
-        let cell3 = row.insertCell();
-        cell1.textContent = S[i];
-        cell2.textContent = sko[i];
-        cell3.textContent = det[i];
+        if (S[i] !== null) {
+            let row = table.insertRow();
+            let cell1 = row.insertCell();
+            let cell2 = row.insertCell();
+            let cell3 = row.insertCell();
+            let cell4 = row.insertCell();
+            cell1.textContent = approximationNames[i]
+            cell2.textContent = S[i];
+            cell3.textContent = sko[i];
+            cell4.textContent = det[i];
+        }
     }
     return table;
 }
@@ -424,9 +458,9 @@ function createFileText(linearCoefs, quadraticCoefs, cubicCoefs, expCoefs, logCo
     text += 'Linear Coefficients: ' + linearCoefs.join(', ') + '\n';
     text += 'Quadratic Coefficients: ' + quadraticCoefs.join(', ') + '\n';
     text += 'Cubic Coefficients: ' + cubicCoefs.join(', ') + '\n';
-    text += 'Exponential Coefficients: ' + expCoefs.join(', ') + '\n';
-    text += 'Logarithmic Coefficients: ' + logCoefs.join(', ') + '\n';
-    text += 'Power Coefficients: ' + powerCoefs.join(', ') + '\n\n';
+    if (expCoefs !== null) text += 'Exponential Coefficients: ' + expCoefs.join(', ') + '\n';
+    if (logCoefs !== null) text += 'Logarithmic Coefficients: ' + logCoefs.join(', ') + '\n';
+    if (powerCoefs !== null) text += 'Power Coefficients: ' + powerCoefs.join(', ') + '\n\n';
 
     text += 'Table:\n';
     text += 'S: ' + S.join(', ') + '\n';
